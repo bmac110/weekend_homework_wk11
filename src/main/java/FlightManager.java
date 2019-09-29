9983fca0b49f38325f2cec4825c7f018ec0c8ef0
Billy Mackay <bossmack50@gmail.com>
2019-09-29 17:08:14 +0100

    finished first extensions

--- src/main/java/FlightManager.java
+++ src/main/java/FlightManager.java
@@ -13,4 +13,16 @@ public class FlightManager {
         int totalBaggageAllowance = flight.getTotalWeight() / 2;
         return totalBaggageAllowance / flight.getCapacity();
     }
+
+    public int baggageWeightUsage(){
+        return this.baggageWeightForPassenger() * flight.passengerCount();
+    }
+
+    public int totalBaggageAllowance(){
+        return flight.getTotalWeight() / 2;
+    }
+
+    public int totalRemainingBaggageAllowance(){
+        return this.totalBaggageAllowance() - this.baggageWeightUsage();
+    }
 }
--- src/test/java/FlightManagerTest.java
+++ src/test/java/FlightManagerTest.java
@@ -8,19 +8,32 @@ public class FlightManagerTest {
     Plane plane;
     Flight flight;
     FlightManager flightManager;
+    Passenger passenger;
 
     @Before
     public void setUp(){
         plane = new Plane(PlaneType.BOEING747);
         flight = new Flight(plane);
         flightManager = new FlightManager(flight);
-
+        passenger = new Passenger("John", 3);
 
     }
-    @Test
-     public void canGetBaggageWeightForPassenger(){
 
+    @Test
+    public void canGetBaggageWeightForPassenger(){
         assertEquals(6, flightManager.baggageWeightForPassenger());
+    }
+
+    @Test
+    public void canGetBaggageWeightUsed(){
+        flight.addPassenger(passenger);
+        assertEquals(6, flightManager.baggageWeightUsage());
+    }
+
+    @Test
+    public void getTotalRemainingBaggageAllowance(){
+        flight.addPassenger(passenger);
+        assertEquals(2494, flightManager.totalRemainingBaggageAllowance());
     }
 
 }
